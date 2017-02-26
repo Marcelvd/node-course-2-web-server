8ea92692c6a064429fa7db049d197e256378c81d
marcelvd <mtvandoremalen@gmail.com>
2017-02-26 15:53:41 +0100

    Added new page projects

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/about', (request, response) => {
     });
 });
 
+app.get('/projects', (request, response) => {
+    response.render('projects.hbs', {
+        pageTitle: 'Project Page',
+    });
+});
+
 app.get('/bad', (request, response) => {
     response.send({
         Err: 'Bad request.',
